ecb229aedaf4a46f5f194753e28b60abac83ac81
Gregory Nickonov <gregoryn@actis.ru>
2020-05-08 10:50:44 +0300

    Skip image if it is not found in registry, but registry has returned it in the list

--- src/docker_registry.rb
+++ src/docker_registry.rb
@@ -70,7 +70,7 @@ class DockerRegistry < Registry
     end
 
     tags
-  rescue RestClient::Unauthorized
+  rescue RestClient::Unauthorized, Faraday::ResourceNotFound
     @logger.error("Unable to get list of tags for repository #{hostname}#{repository}")
 
     []
